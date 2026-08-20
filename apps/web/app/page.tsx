f4c2d952703799ffe7d2e6130bbfad721a6f7f20
asano <asano.s.24kdgn@gmail.com>
2026-08-20 12:33:24 +0900

    トップページのヘッダー・フッターを削除

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,17 +2,14 @@ import Image from "next/image";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ReadMoreButton from "@/components/ReadMoreButton";
-import ReadMoreButton from "@/components/ReadMoreButton";
 
 export default function Home() {
   return (
     <>
-      <Header />
       <main className="pb-16">
         <p>メイン</p>
         <ReadMoreButton/>
       </main>
-      <Footer />
     </>
   );
 }
