f9a3275c39648cf56f514c0e3bea37b8298f7e62
sakurai <kanato25kdgn@gmail.com>
2026-08-21 00:55:01 +0900

    delete: sql file

--- apps/api/src/db/drizzle/0005_spotty_doomsday.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `follows` ADD `created_at` timestamp DEFAULT (now()) NOT NULL;--> statement-breakpoint
-ALTER TABLE `follows` ADD `updated_at` timestamp DEFAULT (now()) NOT NULL;
--- apps/api/src/db/drizzle/0006_lying_the_santerians.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `follows` RENAME COLUMN `userTo` TO `user_to`;--> statement-breakpoint
-ALTER TABLE `follows` DROP INDEX `follow_unique`;--> statement-breakpoint
-ALTER TABLE `follows` DROP FOREIGN KEY `follow_to_FK`;
---> statement-breakpoint
-ALTER TABLE `follows` ADD CONSTRAINT `follow_unique` UNIQUE(`user_from`,`user_to`);--> statement-breakpoint
-ALTER TABLE `follows` ADD CONSTRAINT `follow_to_FK` FOREIGN KEY (`user_to`) REFERENCES `users`(`id`) ON DELETE cascade ON UPDATE no action;
