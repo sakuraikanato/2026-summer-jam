34fc768dc83b78ad82b514b4cbbcf7684ac3b5c5
shinnosuke <tanakashinnosuke1213@gmail.com>
2026-08-20 12:25:41 +0900

    もっと見る＞ボタンの追加

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import ReadMoreButton from "@/components/ReadMoreButton";
 
 export default function Home() {
   return (
     <>
-      <Header/>
+      <Header />
       <main className="pb-16">
         <p>メイン</p>
+        <ReadMoreButton />
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
