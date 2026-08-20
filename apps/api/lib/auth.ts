8899b84fa043568365b52f422837ba1b80faba8d
asano <asano.s.24kdgn@gmail.com>
2026-08-20 16:17:59 +0900

    ログイン機能を追加

--- apps/api/lib/auth.ts
+++ apps/api/lib/auth.ts
@@ -9,6 +9,9 @@ const userRoleSchema = z.enum([
 ])
 
 export const auth = betterAuth({
+    trustedOrigins: [
+      process.env.FRONT_URL ?? "http://localhost:3000",
+    ],
     database: drizzleAdapter(db, {
         provider: "mysql",
     }),
