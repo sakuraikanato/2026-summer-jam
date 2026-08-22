6d4011641d4450b74e629d97092014313dcf05f7
sakurai <kanato25kdgn@gmail.com>
2026-08-22 12:18:18 +0900

    fix: get session handdler

---
+++ apps/api/src/routes/me.ts
@@ -0,0 +1,21 @@
+import { AuthVariables } from "../..//lib/auth";
+import { ApiResponse } from "../..//lib/responseType";
+import { userAuth } from "../..//middlwere/userAuth";
+import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+
+export const me = new Hono<{ Variables: AuthVariables }>()
+
+.use(userAuth)
+.get("/", (c) => {
+  const user = c.get("user")
+
+  console.log(user);
+  if (!user) throw new HTTPException(401, {message: "User Not Found"});
+  
+  return c.json<ApiResponse<typeof user>>({
+    success: true,
+    data: user
+  })
+
+})
